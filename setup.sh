4139b7d494489194bd717740bb98f4e996c514df
Michael Bui <michael.bui_crystal@outlook.com>
2024-11-21 02:27:24 +1300

    syslink dont like me

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@
 
 DOTFILES_DIR="$HOME/.dotfiles"
 
-# List of files/folders to symlink
+# List of files to symlink
 FILES=(
     ".zshrc"
     ".gitconfig"
+    ".gitignore"
     ".vimrc"
     "settings.json"
 )
 
-# Create symlinks for individual files
 for file in "${FILES[@]}"; do
     src="$DOTFILES_DIR/$file"
     dest="$HOME/$file"
@@ -30,22 +30,6 @@ for file in "${FILES[@]}"; do
     ln -s "$src" "$dest"
 done
 
-# Handle the .oh-my-zsh directory
-OH_MY_ZSH_DIR="$DOTFILES_DIR/.oh-my-zsh"
-if [ -d "$OH_MY_ZSH_DIR" ]; then
-    # Backup existing .oh-my-zsh folder if it exists
-    if [ -e "$HOME/.oh-my-zsh" ] || [ -L "$HOME/.oh-my-zsh" ]; then
-        echo "Backing up $HOME/.oh-my-zsh to $HOME/.oh-my-zsh.backup"
-        mv "$HOME/.oh-my-zsh" "$HOME/.oh-my-zsh.backup"
-    fi
-
-    # Create the symlink for the .oh-my-zsh folder
-    echo "Creating symlink for .oh-my-zsh"
-    ln -s "$OH_MY_ZSH_DIR" "$HOME/.oh-my-zsh"
-else
-    echo "No .oh-my-zsh directory found in your dotfiles"
-fi
-
-# Reload Zsh after applying changes
-echo "Reloading Zsh configuration..."
+# Reload Zsh configuration
+echo "Reloading Zsh..."
 exec zsh
